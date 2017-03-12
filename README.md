ea7ec12cfd9abbbadfbd09d13ed1b2ab7e47ff0a
Gilbertsoft <it@gilbertsoft.ch>
2017-03-12 04:18:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<a href="http://gilbertsoft.org/">
+    <img src="http://gilbertsoft.org/fileadmin/images/gilbertsoft.png" alt="Gilbertsoft logo" title="Gilbertsoft" align="right" height="53" width="243" />
+</a>
+
+GS Warranty TYPO3 extension
+===========================
+
+GS Warranty is for internal use only!
---
+++ ext_tables.php
@@ -0,0 +1,54 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function($extKey)
+    {
+		/***************
+		 * Make the extension configuration accessible
+		 */
+		if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey])) {
+			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+		}
+
+		/***************
+		 * Backend Styling
+		 */
+		if (TYPO3_MODE == 'BE') {
+			/**
+			 * Configure TBE_STYLES (TYPO3 = 7)
+			 */
+			$GLOBALS['TBE_STYLES']['logo'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($extKey) . 'Resources/Public/Images/Backend/TopBarLogo@2x.png';
+
+			/**
+			 * Configure Backend Extension
+			 */
+			if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
+				$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
+			}
+			// Login Logo (TYPO3 >= 7)
+			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/LoginLogo.png';
+
+			// Backend Logo (TYPO3 >= 8)
+			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['backendLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/TopBarLogo@2x.png';
+
+			if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
+				$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
+			}
+		}
+
+		/***************
+		 * Reset extConf array to avoid errors
+		 */
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey])) {
+			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$extKey]);
+		}
+
+		/***************
+		 * Add static files
+		 */
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'GS Warranty');
+
+    },
+    $_EXTKEY
+);
