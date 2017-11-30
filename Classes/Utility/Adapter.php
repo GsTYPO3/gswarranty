11a07b8a9261978d9a21153e289d8c2defc11990
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2017-11-30 14:24:28 +0100

    Generation of toolbar item by fluid

--- Classes/Utility/Adapter.php
+++ Classes/Utility/Adapter.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the GS Warranty TYPO3 Extension.
+ * This file is part of the "GS Warranty" Extension for TYPO3 CMS.
  *
  * Copyright (C) 2017 by Gilbertsoft (gilbertsoft.org)
  *
@@ -23,6 +23,9 @@
 
 namespace Gilbertsoft\Warranty\Utility;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 /**
  * GS Warranty adapter class.
@@ -34,129 +37,140 @@ namespace Gilbertsoft\Warranty\Utility;
  */
 class Adapter
 {
-	/**
-	 * @var integer
-	 */
-	protected static $version = null;
+    /**
+     * @var integer
+     */
+    protected static $version = null;
 
-	/**
-	 * Returns the TYPO3 version as integer
-	 *
-	 * @return integer
-	 * @api
-	 */
-	protected static function getTypo3Version()
-	{
-		if (is_null(static::$version)) {
-			static::$version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch);
-		}
-		return static::$version;
-	}
+    /**
+     * Returns the TYPO3 version as integer
+     *
+     * @return integer
+     * @api
+     */
+    protected static function getTypo3Version()
+    {
+        if (is_null(static::$version)) {
+            static::$version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch);
+        }
+        return static::$version;
+    }
 
-	/**
-	 * Returns TRUE if the current TYPO3 version (or compatibility version) is compatible to the input version
-	 * Notice that this function compares branches, not versions (4.0.1 would be > 4.0.0 although they use the same compat_version)
-	 *
-	 * @param string $verNumberStr Minimum branch number required (format x.y / e.g. "4.0" NOT "4.0.0"!)
-	 * @return bool Returns TRUE if this setup is compatible with the provided version number
-	 * @todo Still needs a function to convert versions to branches
-	 */
-	protected static function isVersion($branchNumberStr)
-	{
-		return (static::getTypo3Version() == \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($branchNumberStr));
-	}
+    /**
+     * Returns TRUE if the current TYPO3 version (or compatibility version) is compatible to the input version
+     * Notice that this function compares branches, not versions (4.0.1 would be > 4.0.0 although they use the same compat_version)
+     *
+     * @param string $verNumberStr Minimum branch number required (format x.y / e.g. "4.0" NOT "4.0.0"!)
+     * @return bool Returns TRUE if this setup is compatible with the provided version number
+     * @todo Still needs a function to convert versions to branches
+     */
+    protected static function isVersion($branchNumberStr)
+    {
+        return (static::getTypo3Version() == \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($branchNumberStr));
+    }
 
-	/**
-	 * Returns TRUE if the current TYPO3 version (or compatibility version) is compatible to the input version
-	 * Notice that this function compares branches, not versions (4.0.1 would be > 4.0.0 although they use the same compat_version)
-	 *
-	 * @param string $verNumberStr Minimum branch number required (format x.y / e.g. "4.0" NOT "4.0.0"!)
-	 * @return bool Returns TRUE if this setup is compatible with the provided version number
-	 * @todo Still needs a function to convert versions to branches
-	 */
-	protected static function isCompatVersion($branchNumberStr)
-	{
-		return static::getTypo3Version() >= \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($branchNumberStr);
-	}
+    /**
+     * Returns TRUE if the current TYPO3 version (or compatibility version) is compatible to the input version
+     * Notice that this function compares branches, not versions (4.0.1 would be > 4.0.0 although they use the same compat_version)
+     *
+     * @param string $verNumberStr Minimum branch number required (format x.y / e.g. "4.0" NOT "4.0.0"!)
+     * @return bool Returns TRUE if this setup is compatible with the provided version number
+     * @todo Still needs a function to convert versions to branches
+     */
+    protected static function isCompatVersion($branchNumberStr)
+    {
+        return static::getTypo3Version() >= \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger($branchNumberStr);
+    }
 
-	/**
-	 * Adapt Copyright Warranty
-	 *
-	 * @param string $extKey Extension Key
-	 * @return void
-	 */
-	protected static function adaptWarranty($extKey)
-	{
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['loginCopyrightWarrantyProvider'] = 'Gilbertsoft';
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['loginCopyrightWarrantyURL'] = 'http://gilbertsoft.com/go/typo3/warranty';
-	}
+    /**
+     * Adapt Copyright Warranty
+     *
+     * @param string $extKey Extension Key
+     * @return void
+     */
+    protected static function adaptWarranty($extKey)
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['loginCopyrightWarrantyProvider'] = 'Gilbertsoft';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['loginCopyrightWarrantyURL'] = 'http://gilbertsoft.com/go/typo3/warranty';
+    }
 
-	/**
-	 * Adapt Backend Styling
-	 *
-	 * @param string $extKey Extension Key
-	 * @return void
-	 */
-	protected static function adaptLogos($extKey)
-	{
-		// Configure TBE_STYLES (TYPO3 = 7.6)
-		if (static::isVersion('7.6')) {
-			$GLOBALS['TBE_STYLES']['logo'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($extKey) . 'Resources/Public/Images/Backend/gilbertsoft-t3-topbar@2x.png';
-		}
+    /**
+     * Adapt Backend Styling
+     *
+     * @param string $extKey Extension Key
+     * @return void
+     */
+    protected static function adaptLogos($extKey)
+    {
+        // Configure TBE_STYLES (TYPO3 = 7.6)
+        if (static::isVersion('7.6')) {
+            $GLOBALS['TBE_STYLES']['logo'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($extKey) . 'Resources/Public/Images/Backend/gilbertsoft-t3-topbar@2x.png';
+        }
 
-		// Configure Backend Extension
-		if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
-		}
+        // Configure Backend Extension
+        if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
+        }
 
-		// Login Logo (TYPO3 >= 7.6)
-		if (static::isCompatVersion('7.6')) {
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-login.png';
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginHighlightColor'] = '#004A99';
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginBackgroundImage'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-background.jpg';
-		}
+        // Login Logo (TYPO3 >= 7.6)
+        if (static::isCompatVersion('7.6')) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-login.png';
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginHighlightColor'] = '#004A99';
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginBackgroundImage'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-background.jpg';
+        }
 
-		// Backend Logo (TYPO3 >= 8.7)
-		if (static::isCompatVersion('8.7')) {
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['backendLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-topbar@2x.png';
-		}
+        // Backend Logo (TYPO3 >= 8.7)
+        if (static::isCompatVersion('8.7')) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['backendLogo'] = 'EXT:' . $extKey . '/Resources/Public/Images/Backend/gilbertsoft-t3-topbar@2x.png';
+        }
 
-		if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
-			$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
-		}
-	}
+        if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
+            $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
+        }
+    }
 
-	protected static function registerToolbarItems($extKey)
-	{
-		$GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1511960569] = \Gilbertsoft\Warranty\Backend\ToolbarItems\WarrantyLinkToolbarItem::class;
-	}
+    protected static function registerToolbarItems($extKey)
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1511960569] = \Gilbertsoft\Warranty\Backend\ToolbarItems\WarrantyLinkToolbarItem::class;
+    }
+    
+    protected static function registerIcons($extKey)
+    {
+        $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+        $iconRegistry->registerIcon(
+            'apps-toolbar-menu-warranty',
+            SvgIconProvider::class,
+            ['source' => 'EXT:' . $extKey . '/Resources/Public/Icons/apps-toolbar-menu-warranty.svg']
+        );
+    }
 
-	/**
-	 * Adapts the backend
-	 *
-	 * @return void
-	 * @api
-	 */
-	public static function localconf($extKey)
-	{
-		if ((TYPO3_MODE === 'BE') && Provider::isGilbertsoft())
-		{
-			static::registerToolbarItems($extKey);
-		}
-	}
+    /**
+     * Adapts the backend
+     *
+     * @return void
+     * @api
+     */
+    public static function localconf($extKey)
+    {
+        if ((TYPO3_MODE === 'BE') && Provider::isGilbertsoft())
+        {
+            static::registerToolbarItems($extKey);
+        }
+    }
 
-	/**
-	 * Adapts the backend
-	 *
-	 * @return void
-	 * @api
-	 */
-	public static function tables($extKey)
-	{
-		if ((TYPO3_MODE === 'BE') && Provider::isGilbertsoft())
-		{
-			static::adaptWarranty($extKey);
-			static::adaptLogos($extKey);
-		}
-	}
+    /**
+     * Adapts the backend
+     *
+     * @return void
+     * @api
+     */
+    public static function tables($extKey)
+    {
+        if ((TYPO3_MODE === 'BE') && Provider::isGilbertsoft())
+        {
+            static::registerIcons($extKey);
+            static::adaptWarranty($extKey);
+            static::adaptLogos($extKey);
+        }
+    }
 }
